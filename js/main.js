48276df00cf433a1971913ca8a5d3de5a8ab0891
CP McClure <c.patrick.mcclure@gmail.com>
2022-03-11 13:21:42 -0500

    add carousel movement, update h2 font

--- js/main.js
+++ js/main.js
@@ -1,24 +1,54 @@
-// let slidePosition = 1;
-// slideShow(slidePosition);
+window.addEventListener('load', changeOrder);
 
-// document.querySelector('.forwards').addEventListener('click', plusSlides(1));
-// document.querySelector('.backwards').addEventListener('click', plusSlides(-1));
+setInterval(changeOrder, 6000);
 
-// function plusSlides(n) {
-//     slideShow(slidePosition += n)
-// }
+function changeOrder() {
+  const allSlides = document.querySelectorAll(".singleSlide");
+  const previous = "1";
+  const current = "2";
+  const next = "3";
+  const offPage1 = "4";
+  const offPage2 = "5";
+  const offPage3 = "6";
+  const offPage4 = "7";
+  const offPage5 = "8";
+  const offPage6 = "9";
+  const offPage7 = "10";
 
-// function currentSlide(n) {
-//     slideShow(slidePosition = n)
-// }
+  for (const slide of allSlides) {
+    const order = slide.getAttribute("data-order");
 
-// function slideShow(n) {
-//     let i;
-//     let slides = document.querySelectorAll('.carPhoto');
-//     if(n > slides.length) {slidePosition = 1};
-//     if(n < 1) {slidePosition = slides.length};
-//     for(i = 0; i < slides.length; i++) {
-//         slides[i].classList.add('hidden');
-//     }
-//     slides[slidePosition-1].classList.remove('hidden');
-// }
+    switch (order) {
+        case current:
+            slide.setAttribute("data-order", next);
+            break;
+        case next:
+            slide.setAttribute("data-order", offPage1);
+            break;
+        case previous:
+            slide.setAttribute("data-order", current);
+            break;
+        case offPage1:
+            slide.setAttribute("data-order", offPage2)
+            break;
+        case offPage2:
+            slide.setAttribute("data-order", offPage3)
+            break;
+        case offPage3:
+            slide.setAttribute("data-order", offPage4)
+            break;
+        case offPage4:
+            slide.setAttribute("data-order", offPage5)
+            break;
+        case offPage5:
+            slide.setAttribute("data-order", offPage6)
+            break;
+        case offPage6:
+            slide.setAttribute("data-order", offPage7)
+            break;
+        case offPage7:
+            slide.setAttribute("data-order", previous)
+            break;
+    }
+  }
+}
